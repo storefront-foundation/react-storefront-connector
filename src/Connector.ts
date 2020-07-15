e55373c63081ea9f3012a478c1670b8743d18e32
Egor Mesyats <egor.mesyats@moovweb.com>
2020-07-15 16:29:46 +0300

    updateCart, removeCartItem

--- src/Connector.ts
+++ src/Connector.ts
@@ -12,7 +12,7 @@ import CmsSlots from './CmsSlots'
 import ProductSlotsParams from './ProductSlotsParams'
 import Route from './Route'
 import CartResponse from './CartResponse'
-import Cart from './Cart'
+import CartItem from './CartItem'
 
 /**
  * The main connector interface. An implementation of this interface should be the default export from your
@@ -81,7 +81,21 @@ export default interface Connector {
   /**
    * Updates the products in the cart
    */
-  updateCart: (cart: Cart, req: Request, response: Response) => Promise<CartResponse>
+  updateCart: (
+    item: CartItem,
+    quantity: number,
+    req: Request,
+    response: Response
+  ) => Promise<CartResponse>
+
+  /**
+   * Removes item in the cart
+   */
+  removeCartItem: (
+    item: CartItem,
+    req: Request,
+    response: Response
+  ) => Promise<CartResponse>
 
   /**
    * Searches for matching products
